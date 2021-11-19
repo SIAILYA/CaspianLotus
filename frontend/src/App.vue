30d42b6e41e4e5882f51b475a2c3efead7b645ac
Anton <texturich@gmail.com>
2021-11-19 23:05:48 +0300

    Хедер начало

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,9 +1,6 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
+    <Header />
     <router-view/>
   </div>
 </template>
@@ -12,3 +9,9 @@
 @import "main";
 
 </style>
+<script>
+import Header from "@/components/Header";
+export default {
+  components: {Header}
+}
+</script>
---
+++ frontend/src/components/Header.vue
@@ -0,0 +1,51 @@
+<template>
+  <b-container>
+    <b-navbar toggleable="lg" type="light" variant="light" class="header">
+      <b-navbar-brand href="#" class="header-logo">
+        <div class="d-flex flex-row">
+          <b-img src="@/assets/lotos_logo.svg" />
+          Каспийский<br>Лотос
+        </div>
+      </b-navbar-brand>
+      <b-navbar-nav>
+        <b-nav-item href="#" class="nav-item">О нас</b-nav-item>
+        <b-nav-item href="#" class="nav-item">Фотоотчёты</b-nav-item>
+        <b-nav-item href="#" class="nav-item">Досуг</b-nav-item>
+        <b-nav-item href="#" class="nav-item">Рыбалка</b-nav-item>
+        <b-nav-item href="#" class="nav-item">Отзывы</b-nav-item>
+        <b-nav-item href="#" class="nav-item">Карта развития</b-nav-item>
+        <b-nav-item href="#" class="nav-item">Контакты</b-nav-item>
+      </b-navbar-nav>
+    </b-navbar>
+  </b-container>
+</template>
+
+<script>
+export default {
+  name: "Header"
+}
+</script>
+
+<style scoped lang="scss">
+.header {
+  background-color: inherit;
+
+  .header-logo {
+    font-family: Montserrat, sans-serif;
+    font-style: normal;
+    font-weight: 500;
+    font-size: 24px;
+    line-height: 1.5;
+    color: #0073A3;
+  }
+
+  .nav-item {
+    font-family: Montserrat, sans-serif;
+    font-style: normal;
+    font-weight: 300;
+    font-size: 18px;
+    line-height: 22px;
+    color: #272727;
+  }
+}
+</style>
