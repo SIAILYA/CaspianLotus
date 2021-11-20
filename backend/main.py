0d149c7c7541bb3c93aac806c640c8df578455fe
tvkoz <tv.kozlov.2002@gmail.com>
2021-11-21 01:59:05 +0300

    Backend: add photos + change of price

--- backend/main.py
+++ backend/main.py
@@ -1,24 +1,27 @@
+from flask import Flask, render_template, request, redirect, url_for, jsonify, flash
+from pymongo import MongoClient
 import datetime
 import os
-
-from bson.json_util import dumps
-from bson.objectid import ObjectId
-from flask import Flask, render_template, request, redirect, url_for
-from pymongo import MongoClient
-
 from forms import *
+from bson.objectid import ObjectId
+from bson.json_util import dumps
 from flask_cors import CORS
+from werkzeug.utils import secure_filename
+from bson.json_util import loads
+
+UPLOAD_FOLDER = 'static/photos/'
+ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg'}
 
 app = Flask(__name__)
+
 SECRET_KEY = os.urandom(32)
 app.config['SECRET_KEY'] = SECRET_KEY
 cors = CORS(app)
 app.config['CORS_HEADERS'] = 'Content-Type'
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
-client = MongoClient(
-    "mongodb+srv://MindlessDoc:K.,k.Nbntxrb228!@cluster0.ctgti.mongodb.net/myFirstDatabase?"
-    "retryWrites=true&w=majority&tlsAllowInvalidCertificates=true"
-)
+
+client = MongoClient("mongodb+srv://MindlessDoc:K.,k.Nbntxrb228!@cluster0.ctgti.mongodb.net/myFirstDatabase?retryWrites=true&w=majority")
 
 db_name = "Lotos"
 
@@ -26,6 +29,7 @@ collections_admins = client[db_name]["PhotoReports"]
 collections_reviews = client[db_name]["reviews"]
 collections_prices = client[db_name]["prices"]
 collections_booking = client[db_name]["booking"]
+collections_photos = client[db_name]["photos"]
 collections_orders = client[db_name]["orders"]
 
 
@@ -33,21 +37,9 @@ collections_orders = client[db_name]["orders"]
 def status():
     return "Caspian Lotus API is working"
 
-
 @app.route('/admin', methods=["GET", "POST"])
 def index():
     return render_template("admin/header.html")
-
-
-@app.route('/test', methods=["GET", "POST"])
-def test():
-    form = MessageForm()
-    if form.validate_on_submit():
-        print(form.name.data)
-        print(form.review.data)
-        return redirect(url_for("test"))
-    return render_template("reviews/templates/admin/addreview.html", form=form)
-
 
 @app.route('/admin/reviews', methods=["GET", "POST"])
 def reviews():
@@ -88,7 +80,6 @@ def add_review():
     })
     return "OK"
 
-
 @app.route('/admin/addreview', methods=["GET", "POST"])
 def add_admin_review():
     form = AddReviewForm()
@@ -99,7 +90,6 @@ def add_admin_review():
         })
         return redirect(url_for("reviews"))
     return render_template("admin/addreview.html", form=form)
-
 
 @app.route('/admin/prices', methods=["GET", "POST"])
 def prices():
@@ -114,6 +104,11 @@ def edit_price(id):
         edit_price_form.count.data = price["count"]
 
     if edit_price_form.validate_on_submit():
+        if edit_price_form.validate_on_submit():
+            if "delete" in request.form:
+                collections_prices.remove({"_id": ObjectId(id)})
+                return redirect(url_for("prices"))
+
         collections_prices.update_one({"_id": ObjectId(id)}, {"$set": {
             "name": edit_price_form.name.data,
             "count": int(edit_price_form.count.data)
@@ -126,19 +121,65 @@ def edit_price(id):
 def get_prices():
     return dumps(collections_prices.find())
 
+@app.route('/admin/addprice', methods=["GET", "POST"])
+def add_admin_price():
+    form = EditPriceForm()
+    if form.validate_on_submit():
+        collections_prices.insert_one({
+            "name": form.name.data,
+            "count": int(form.count.data)
+        })
+        return redirect(url_for("prices"))
+    return render_template("admin/addprice.html", form=form)
 
-# @app.route('/addreview', methods=["GET", "POST"])
-# def add_review():
-#     form = AddReviewForm()
-#     if form.validate_on_submit():
-#         name = form.name.data
-#         review = form.review.data
-#         collections_reviews.insert_one({
-#             "name": name,
-#             "review": review
-#         })
-#         return redirect(url_for("reviews"))
-#     return render_template("reviews/addreview.html", form=form)
+
+@app.route('/admin/photos', methods=["GET", "POST"])
+def photos():
+    return render_template("admin/photos.html", photos=collections_photos.find())
+
+
+@app.route('/admin/edit_photo/<id>', methods=["GET", "POST"])
+def edit_photo(id):
+    edit_photo_form = EditPriceForm()
+    filename = collections_photos.find({"_id": ObjectId(id)})[0]
+    if "delete" in request.form:
+        os.remove('static/' + filename["way"])
+        collections_photos.remove({"_id": ObjectId(id)})
+        return redirect(url_for("photos"))
+
+    return render_template("admin/edit_photos.html", edit_photo_form=edit_photo_form, filename=filename["way"])
+
+
+def add_admin_photo():
+    add_photo_form = AddPhotoForm()
+    if add_photo_form.validate_on_submit():
+        photo = add_photo_form.photo
+        if photo and allowed_file(photo.data.filename):
+            filename = secure_filename(photo.data.filename)
+            photo.data.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            collections_photos.insert_one({
+                'way': 'photos/' + filename
+            })
+            return redirect(url_for("photos"))
+    return render_template("admin/addphoto.html", add_photo_form=add_photo_form)
+
+def allowed_file(filename):
+    return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+
+@app.route('/admin/addphoto', methods=["GET", "POST"])
+def add_admin_photo():
+    add_photo_form = AddPhotoForm()
+    if add_photo_form.validate_on_submit():
+        photo = add_photo_form.photo
+        if photo and allowed_file(photo.data.filename):
+            filename = secure_filename(photo.data.filename)
+            photo.data.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            collections_photos.insert_one({
+                'way': 'photos/' + filename
+            })
+            return redirect(url_for("photos"))
+    return render_template("admin/addphoto.html", add_photo_form=add_photo_form)
 
 
 @app.post("/api/check_booking")
